080eaaf4f10a2a43b0a8773a302f9e86052320af
Monoworks <r.sato.job@gmail.com>
2023-03-09 19:36:50 +0900

    諸々追加

--- trpg/assets/script/script.js
+++ trpg/assets/script/script.js
@@ -1,66 +1,94 @@
-  // header・footer共通パーツ化
+// header・footer共通パーツ化
 
-  function include_header(rootDir) {
-    $.ajax({
-      url: rootDir + 'assets/parts/header.html', // リクエストを送信するURLを指定
-      async: false, // 非同期リクエストを無効にする
-    }).done(function (header_html) { // 通信が成功したら
-      header_html = header_html.replace(/\{\$root\}/g, rootDir); // header.htmlの文字列を置き換え
-      document.write(header_html); // herder.htmlを表示する
-    });
-  }
-
-  function include_footer(rootDir) {
-    $.ajax({
-      url: rootDir + 'assets/parts/footer.html', // リクエストを送信するURLを指定
-      async: false, // 非同期リクエストを無効にする
-    }).done(function (footer_html) { // 通信が成功したら
-      footer_html = footer_html.replace(/\{\$root\}/g, rootDir); // footer.htmlの文字列を置き換え
-      document.write(footer_html); // footer.htmlを表示する
-    });
-  }
-
-  $(function () {
-
-    // ページ内移動アニメーション
-    $('a[href^="#"]').click(function () {
-      var adjust = 0;
-      var speed = 400;
-      var href = $(this).attr("href");
-      var target = $(href == "#" || href == "" ? "html" : href);
-      var position = target.offset().top + adjust;
-      $("body,html").animate({
-        scrollTop: position
-      }, speed, "swing");
-      return false;
-    });
-    //フェードアニメーション
-    $(window).scroll(function () {
-      const windowHeight = $(window).height();
-      const scroll = $(window).scrollTop();
-
-      $(".fadeIn").each(function () {
-        const targetPosition = $(this).offset().top;
-        if (scroll > targetPosition - windowHeight + 100) {
-          $(this).addClass("is-show");
-        }
-      });
-    });
+function include_header(rootDir) {
+  $.ajax({
+    url: rootDir + 'assets/parts/header.html', // リクエストを送信するURLを指定
+    async: false, // 非同期リクエストを無効にする
+  }).done(function (header_html) { // 通信が成功したら
+    header_html = header_html.replace(/\{\$root\}/g, rootDir); // header.htmlの文字列を置き換え
+    document.write(header_html); // herder.htmlを表示する
   });
+}
 
-  // iframe設定
-  $(window).on("message", function (e) {
-    $(function () {
-      if ($("#goodsmenuframe").length !== 0) {
-        $("#goodsmenuframe iframe").css({
-          height: e.originalEvent.data + 4 + "px",
-          border: "none",
-        });
+function include_footer(rootDir) {
+  $.ajax({
+    url: rootDir + 'assets/parts/footer.html', // リクエストを送信するURLを指定
+    async: false, // 非同期リクエストを無効にする
+  }).done(function (footer_html) { // 通信が成功したら
+    footer_html = footer_html.replace(/\{\$root\}/g, rootDir); // footer.htmlの文字列を置き換え
+    document.write(footer_html); // footer.htmlを表示する
+  });
+}
+
+$(function () {
+
+  // ページ内移動アニメーション
+  $('a[href^="#"]').click(function () {
+    var adjust = 0;
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top + adjust;
+    $("body,html").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+  //フェードアニメーション
+  $(window).scroll(function () {
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+
+    $(".fadeIn").each(function () {
+      const targetPosition = $(this).offset().top;
+      if (scroll > targetPosition - windowHeight + 100) {
+        $(this).addClass("is-show");
       }
     });
   });
+});
 
-  // 遅延読み込み
-  jQuery(function ($) {
-    $("img.lazy").lazyload();
+// iframe設定
+$(window).on("message", function (e) {
+  $(function () {
+    if ($("#goodsmenuframe").length !== 0) {
+      $("#goodsmenuframe iframe").css({
+        height: e.originalEvent.data + 4 + "px",
+        border: "none",
+      });
+    }
   });
+});
+
+// 遅延読み込み
+jQuery(function ($) {
+  $("img.lazy").lazyload();
+});
+
+$(function () {
+  $('#openModal').click(function () {
+    $('#modalArea').fadeIn();
+  });
+  $('#closeModal , #modalBg').click(function () {
+    $('#modalArea').fadeOut();
+  });
+});
+
+const mySwiper = new Swiper('.card02 .swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  grabCursor: true,
+  pagination: {
+    el: '.card02 .swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.card02 .swiper-button-next',
+    prevEl: '.card02 .swiper-button-prev',
+  },
+  breakpoints: {
+    1025: {
+      spaceBetween: 32,
+    }
+  },
+});
