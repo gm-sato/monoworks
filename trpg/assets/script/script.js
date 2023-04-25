7dfbd00f8fd017f1f58211ed14f76775746361ba
Monoworks <r.sato.job@gmail.com>
2023-04-25 17:15:04 +0900

    CHARACTER　タグ機能実装

--- trpg/assets/script/script.js
+++ trpg/assets/script/script.js
@@ -128,3 +128,78 @@ window.addEventListener('scroll', function () {
     }
   }
 });
+
+
+window.addEventListener("load", function () {
+  const d = document;
+  const items = d.querySelectorAll(".js-item");
+  const terms = [];
+
+  for (let i in items) {
+    for (let i = 0; i < items.length; i++) {
+      if (i < 8) {
+        items[i].classList.add("accept");
+      } else {
+        items[i].classList.add("hidden");
+      }
+    }
+  }
+
+  const buttons = d.querySelectorAll(".js-item-term");
+  for (const button of buttons) {
+    button.addEventListener("click", function () {
+      button.classList.toggle("active");
+
+      // クリックした条件を絞り込み条件リストに追加/削除
+      const term = button.getAttribute("data-term");
+      const termPosition = terms.indexOf(term);
+      if (termPosition !== -1) {
+        terms.splice(termPosition, 1);
+      } else {
+        terms.push(term);
+      }
+      console.log("terms: ", terms);
+
+      // 絞り込み
+      for (const item of items) {
+        for (let i in items) {
+          for (let i = 0; i < items.length; i++) {
+            item.classList.remove("accept");
+            item.classList.remove("hidden");
+          }
+        }
+        // 選択されてる条件ないなら全てのfadeOut取って処理終了
+        if (terms.length === 0 && item.classList.contains("fadeOut")) {
+          item.classList.remove("fadeOut");
+          continue;
+        }
+        // 空白削除して配列に
+        const itemTagsArray = item
+          .getAttribute("data-tag")
+          .replace(/\s+/g, "")
+          .split(",");
+        const isIncludedTerm = terms.filter(function (term) {
+          return itemTagsArray.indexOf(term) !== -1;
+        });
+        if (isIncludedTerm.length !== terms.length && !item.classList.contains("fadeOut")) {
+          // 現在の条件と完全一致してなくてfadeOutクラスもなければfadeOutクラス付与
+          item.classList.add("fadeOut");
+        } else if (isIncludedTerm.length === terms.length && item.classList.contains("fadeOut")) {
+          // 現在の条件と完全一致しててfadeOutクラスあるならfadeOutクラス外す
+          item.classList.remove("fadeOut");
+        }
+      }
+      let arritem = document.querySelectorAll('.chara-box:not(.fadeOut)');
+      for (let i in arritem) {
+        console.log(arritem[i]);
+        for (let i = 0; i < arritem.length; i++) {
+          if (i < 8) {
+            arritem[i].classList.add("accept");
+          } else {
+            arritem[i].classList.add("hidden");
+          }
+        }
+      }
+    });
+  }
+});
