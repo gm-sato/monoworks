68737910ea252e1229e558c333d21ac8acadbf13
Monoworks <r.sato.job@gmail.com>
2025-03-12 18:01:49 +0900

    Jerico制作開始

---
+++ contents/jerico/src/views/About.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+div
+  h1 HO1のページです
+  p このページでは、私たちのプロジェクトについて詳しく説明しています。
+  router-link(to="/") ホームページへ
+</template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #555;
+}
+p {
+  font-size: 16px;
+  line-height: 1.5;
+}
+</style>
---
+++ contents/jerico/src/views/Home.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+div.l-bg
+  article
+    section.l-hero-image
+      h1 楽園‐Jerico‐
+    section.l-story
+      .l-story__container
+        .l-story__text
+          p 異能発現統制機関
+          p 通称<br>『ガーデン』
+          p そこに咲く花はない。<br>ただ冷たい器に収められた、<br>未成熟な命があるだけだった。
+          p 壁の向こうには、何があるのか。
+          p それを知る者はいない。<br>夢や希望は幻に過ぎず、<br>現実は冷酷で無情なままだ。
+          p それでも、誰かが囁く。
+          p 「この壁の向こうに、きっとあるの。私たちの—」
+          p ダブルクロスThe 3rd Edition
+          p 楽園 <br>-Jericho-
+          p ダブルクロス—それは裏切りを意味する言葉。
+
+    section.l-handout
+      .c-headingType2
+        h2 HANDOUT
+      .l-handout__container
+        p.l-handout__container-text キミたちは捨てられた存在だ。
+          br
+          | 物心ついた頃にこの施設へと移り、施設で育ってきた。
+          br
+          | 君たちの目的はただ一つ、『ガーデン』での戦闘訓練を生活として送っている。
+          br
+          | キミたちの目的はただ一つ、『適性ランク』を上げることだけだ。
+      .l-handout__list
+        .l-handout__card
+          img.l-handout__card-mv(src="../assets/images/handout.png" alt="カード画像")
+          h3.l-handout__card-title タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          p.l-handout__card-text テキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        .l-handout__card
+          img.l-handout__card-mv(src="../assets/images/handout.png" alt="カード画像")
+          h3.l-handout__card-title タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          p.l-handout__card-text テキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        .l-handout__card
+          img.l-handout__card-mv(src="../assets/images/handout.png" alt="カード画像")
+          h3.l-handout__card-title タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+          p.l-handout__card-text テキストテキストテキストテキストテキストテキストテキストテキストテキスト
+
+
+    section.l-character
+      .c-headingType2
+        h2 CHARACTER
+      .l-character__wrapper
+        ul.l-character__list
+          li(data-number="1").l-character__list-item: img(src="../assets/images/character.png")
+          li(data-number="2").l-character__list-item: img(src="../assets/images/character.png")
+          li(data-number="3").l-character__list-item: img(src="../assets/images/character.png")
+          li(data-number="4").l-character__list-item: img(src="../assets/images/character.png")
+          li(data-number="5").l-character__list-item: img(src="../assets/images/character.png")
+          li(data-number="6").l-character__list-item: img(src="../assets/images/character.png")
+
+    section.l-log
+      .l-log_wrapper
+
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped></style>
